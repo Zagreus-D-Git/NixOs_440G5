b9540a602d9fe0325249e3c17ff65c1e5d31ba22
Zagreus-D-Git <hardbreakconque@gmail.com>
2026-05-13 11:31:19 -0600

    no user packages in config

--- configuration.nix
+++ configuration.nix
@@ -93,8 +93,8 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
-  brave
-  spotify
+  #brave
+  #spotify
   usbutils
   pciutils
   htop
